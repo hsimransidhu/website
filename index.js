1620f679aa9118ecefc00173562cef17803d5495
hsimransidhu <simusidhu2004@gmail.com>
2023-11-09 19:02:40 -0600

    add file

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//utility functions 
+function select(selector) {
+    return document.querySelector(selector)
+}
+let projectName = 'Super Pizza';
+let address = 'henlow bay';
+ function printMenu() {
+    // fetch the menu
+    //forEach..
+
+ }
